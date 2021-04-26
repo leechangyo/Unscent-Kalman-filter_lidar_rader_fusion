b6ab001e1e11547cd716000d07d009229df517ee
leechangyo <lcgyo@naver.com>
2021-04-26 14:19:09 +0800

    first commit

---
+++ 2.Unscented Kalman Filters_Augmentation copy/ukf.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include "ukf.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+UKF::UKF() {
+  Init();
+}
+
+UKF::~UKF() {
+
+}
+
+void UKF::Init() {
+
+}
+
+
+/**
+ * Programming assignment functions: 
+ */
+
+void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
+
+  // set state dimension
+  int n_x = 5;
+
+  // set augmented dimension
+  int n_aug = 7;
+
+  // Process noise standard deviation longitudinal acceleration in m/s^2
+  double std_a = 0.2;
+
+  // Process noise standard deviation yaw acceleration in rad/s^2
+  double std_yawdd = 0.2;
+
+  // define spreading parameter
+  double lambda = 3 - n_aug;
+
+  // set example state
+  VectorXd x = VectorXd(n_x);
+  x <<   5.7441,
+         1.3800,
+         2.2049,
+         0.5015,
+         0.3528;
+
+  // create example covariance matrix
+  MatrixXd P = MatrixXd(n_x, n_x);
+  P <<     0.0043,   -0.0013,    0.0030,   -0.0022,   -0.0020,
+          -0.0013,    0.0077,    0.0011,    0.0071,    0.0060,
+           0.0030,    0.0011,    0.0054,    0.0007,    0.0008,
+          -0.0022,    0.0071,    0.0007,    0.0098,    0.0100,
+          -0.0020,    0.0060,    0.0008,    0.0100,    0.0123;
+
+  // create augmented mean vector
+  VectorXd x_aug = VectorXd(7);
+
+  // create augmented state covariance
+  MatrixXd P_aug = MatrixXd(7, 7);
+
+  // create sigma point matrix
+  MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
+
+  /**
+   * Student part begin
+   */
+ 
+  // create augmented mean state
+  x_aug.head(5) = x;
+  x_aug(5) = 0; // mu_a(noise)
+  x_aug(6) = 0; // mu_(noise)
+
+  // create augmented covariance matrix
+  P_aug.fill(0.0);
+  P_aug.topLeftCorner(5,5)=P;
+  
+  // Add Q(covariance of standard deviation of accelration and yaw acceraltion) to Augumented Covaraince matrix(consider sigma point);
+  P_aug(5,5) = std_a*std_a;
+  P_aug(6,6) = std_yawdd*std_yawdd;
+
+  // create square root matrix
+  MatrixXd A = P_aug.llt().matrixL();
+
+  // create augmented sigma points
+  Xsig_aug.col(0) = x_aug;
+  
+  for(int i = 0; i<n_aug; ++i)
+  {
+      Xsig_aug.col(i+1) = x_aug + sqrt(lambda+n_aug)*A.col(i);
+      Xsig_aug.col(n_aug+i+1) = x_aug - sqrt(lambda+n_aug)*A.col(i);
+  }
+  
+  /**
+   * Student part end
+   */
+
+  // print result
+  std::cout << "Xsig_aug = " << std::endl << Xsig_aug << std::endl;
+
+  // write result
+  *Xsig_out = Xsig_aug;
+}
